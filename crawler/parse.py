b385c34b3691132c7f3ecaa18a1596fcc826826b
D3lux3 <dani.haapaniemi@hotmail.com>
2023-09-13 21:45:06 +0300

    Cleaner

--- crawler/parse.py
+++ crawler/parse.py
@@ -6,17 +6,23 @@ with open('test.json') as f:
 videos = data['contents']['twoColumnSearchResultsRenderer']['primaryContents'][
     'sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents']
 
-for video in videos:
-    if 'videoRenderer' not in video:
-        continue
+# Maybe faster to implement regex that gets contents instead of the whole JSON.
 
-    videoId = video['videoRenderer']['videoId']
-    title = video['videoRenderer']['title']['runs'][0]['text']
-    view_count = video['videoRenderer']['viewCountText']['simpleText']
-    length = video['videoRenderer']['lengthText']['simpleText']
+videos_2 = ({
+    "videodId": vid['videoRenderer']['videoId'],
+    "title": vid['videoRenderer']['title']['runs'][0]['text'],
+    "view_count": vid['videoRenderer']['viewCountText']['simpleText'],
+    "length": vid['videoRenderer']['lengthText']['simpleText'],
+    "channel": vid['videoRenderer']['ownerText']['runs'][0]['navigationEndpoint']['commandMetadata']['webCommandMetadata']['url']
+}
+    for vid in videos if 'videoRenderer' in vid)
 
+
+for v in list(videos_2):
+    videoId, title, view_count, channel, length = v.values()
     print(f"Video ID: {videoId}")
     print(f"Title: {title}")
     print(f"View Count: {view_count}")
+    print(f"Channel: {channel}")
     print(f"Length: {length}")
     print()
