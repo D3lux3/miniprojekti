99d5b306e6d8ebca12a0c74e1becd800384dc81f
D3lux3 <dani.haapaniemi@hotmail.com>
2023-09-14 13:39:53 +0300

    Default values when JSON properties are not available.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -3,12 +3,15 @@ import re
 import json
 from bs4 import BeautifulSoup
 import csv
-import os, os.path
+import os
+import os.path
+
 
 def get_amount_of_files_in_outputs():
     return len([name for name in os.listdir('./outputs/') if os.path.isfile(os.path.join('./outputs/', name))])
 
-#TODO: Logic that handles situations where the query fails. Message or re-run.
+# TODO: Logic that handles situations where the query fails. Message or re-run.
+
 
 def start(filename):
     search_words_file = open(filename, 'r')
@@ -36,6 +39,23 @@ def get_video_data_by_word(query):
                 parse_json_to_csv_file(query, f"{'{'}{matches[0]}{'}'}")
 
 
+def map_video_data(video):
+    video_renderer = video.get('videoRenderer', {})
+    vid_id = video_renderer.get('videoId', '')
+    title = video_renderer.get('title', {}).get('runs', [{}])[0].get('text', '')
+    view_count = video_renderer.get('viewCountText', {}).get('simpleText', '')
+    length = video_renderer.get('lengthText', {}).get('simpleText', '')
+    owner_text = video_renderer.get('ownerText', {}).get('runs', [{}])[0]
+    channel = owner_text.get('navigationEndpoint', {}).get('commandMetadata', {}).get('webCommandMetadata', {}).get('url', '')
+    return {
+        "Video Id": vid_id,
+        "Title": title,
+        "View Count": view_count,
+        "Length": length,
+        "Channel": channel
+    }
+
+
 def parse_json_to_csv_file(query, json_data):
     print(f'Starting to save query: {query}')
     data = json.loads(json_data)
@@ -45,18 +65,12 @@ def parse_json_to_csv_file(query, json_data):
     video_data = data['contents']['twoColumnSearchResultsRenderer']['primaryContents'][
         'sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents']
 
-    videos = ({
-        "Video Id": vid['videoRenderer']['videoId'],
-        "Title": vid['videoRenderer']['title']['runs'][0]['text'],
-        "View Count": vid['videoRenderer']['viewCountText']['simpleText'],
-        "Length": vid['videoRenderer']['lengthText']['simpleText'],
-        "Channel": vid['videoRenderer']['ownerText']['runs'][0]['navigationEndpoint']['commandMetadata']['webCommandMetadata']['url']
-    }
-        for vid in video_data if 'videoRenderer' in vid)
-    
+    videos = (map_video_data(vid) for vid in video_data if 'videoRenderer' in vid)
+
     with open(f'outputs/{query}.csv', 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=field_names)
         writer.writeheader()
         writer.writerows(videos)
+
 
 start('./20k.txt')
