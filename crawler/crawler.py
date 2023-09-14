422894def0d6ee20641bd681de87bbcdceea902a
D3lux3 <dani.haapaniemi@hotmail.com>
2023-09-14 12:58:59 +0300

    Merged parse logic into crawler, and improved crawler logic.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -2,24 +2,61 @@ import requests
 import re
 import json
 from bs4 import BeautifulSoup
+import csv
+import os, os.path
 
-x = requests.get('https://www.youtube.com/results?search_query=asd')
+def get_amount_of_files_in_outputs():
+    return len([name for name in os.listdir('./outputs/') if os.path.isfile(os.path.join('./outputs/', name))])
 
-soup = BeautifulSoup(x.content, 'html.parser')
+#TODO: Logic that handles situations where the query fails. Message or re-run.
 
-pattern = 'var\s+ytInitialData\s*=\s*{(.*?)};'
+def start(filename):
+    search_words_file = open(filename, 'r')
+    search_words = search_words_file.readlines()
+    file_amount = get_amount_of_files_in_outputs()
+
+    for word in search_words[file_amount:]:
+        get_video_data_by_word(word.strip())
 
 
-script_tags = soup.find_all('script')
+def get_video_data_by_word(query):
+    x = requests.get(f"https://www.youtube.com/results?search_query={query}")
+    soup = BeautifulSoup(x.content, 'html.parser')
 
-for script_tag in script_tags:
-    script_content = script_tag.string
+    script_tags = soup.find_all('script')
 
-    if script_content:
-        # Use regular expressions to search for the JavaScript object
-        matches = re.findall(r'var\s+ytInitialData\s*=\s*{(.*?)};', script_content, re.DOTALL)
+    for script_tag in script_tags:
+        script_content = script_tag.string
 
-        if len(matches) == 1:
-            f = open("test.json", "a")
-            f.write(f"{'{'}{matches[0]}{'}'}")
-            f.close()
+        if script_content:
+            matches = re.findall(
+                r'var\s+ytInitialData\s*=\s*{(.*?)};', script_content, re.DOTALL)
+
+            if len(matches) == 1:
+                parse_json_to_csv_file(query, f"{'{'}{matches[0]}{'}'}")
+
+
+def parse_json_to_csv_file(query, json_data):
+    print(f'Starting to save query: {query}')
+    data = json.loads(json_data)
+
+    field_names = ['Video Id', 'Title', 'View Count', 'Length', 'Channel']
+
+    video_data = data['contents']['twoColumnSearchResultsRenderer']['primaryContents'][
+        'sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents']
+
+    videos = ({
+        "Video Id": vid['videoRenderer']['videoId'],
+        "Title": vid['videoRenderer']['title']['runs'][0]['text'],
+        "View Count": vid['videoRenderer']['viewCountText']['simpleText'],
+        "Length": vid['videoRenderer']['lengthText']['simpleText'],
+        "Channel": vid['videoRenderer']['ownerText']['runs'][0]['navigationEndpoint']['commandMetadata']['webCommandMetadata']['url']
+    }
+        for vid in video_data if 'videoRenderer' in vid)
+    
+    with open(f'outputs/{query}.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        writer.writeheader()
+        writer.writerows(videos)
+
+start('./20k.txt')
