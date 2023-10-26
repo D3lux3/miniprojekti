095c4ce39300231fbf8d54b6f702865ff2b6c3cf
Topias Harjunpää <topias.harjunpaa@gmail.com>
2023-10-26 20:04:14 +0300

    create word ranker using word rank list

--- src/app.py
+++ src/app.py
@@ -1,10 +1,19 @@
 import random
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import pandas as pd
+import csv
+import re
+from nltk.stem.porter import *
 
-file_paths = ["wrangler/wrangled_data_part1.csv", "wrangler/wrangled_data_part2.csv"]
-df = pd.concat([pd.read_csv(path) for path in file_paths], ignore_index=True)
+with open("eda/word_rank.csv", mode="r") as file:
+    r = csv.reader(file)
+    word_rank = {row[0]: float(row[1]) for row in r}
+
+def rank_title(input):
+    print(input)
+    input = re.sub(r"[^\w\s]", "", input.lower()) # remove punctuations and change to lower
+    stemmed_words = [PorterStemmer().stem(word) for word in str(input).split()] # stemming
+    return sum(word_rank.get(word, 0) for word in stemmed_words)
 
 app = Flask(__name__)
 CORS(app, origins="http://localhost:5173", supports_credentials=True)
@@ -14,21 +23,11 @@ CORS(app, origins="http://localhost:5173", supports_credentials=True)
 def get_message():
     return jsonify(message="Hello")
 
-@app.route("/randrank", methods=["GET"])
-def get_random_rank():
-    return jsonify(rank=round(random.uniform(0.00, 1.00), 2))
-
-@app.route("/", methods=["POST"])
-def update_message():
+@app.route("/rank", methods=["POST"])
+def rank_input_title():
     data = request.json
-    word = data.get("input_word", "")
-    print(word)
-    return jsonify(message=f"Cool {word} video")
-
-@app.route("/data", methods=["GET"])
-def get_sample():
-    sample = df.head().to_json(orient="records")
-    return jsonify(data=sample)
+    title = data.get("text", "")
+    return jsonify(rank=rank_title(title))
 
 
 if __name__ == "__main__":
