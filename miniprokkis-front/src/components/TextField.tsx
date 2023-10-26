f7a28368852a3aaf811372e9f1f3d91e3cebcc3b
Topias Harjunpää <topias.harjunpaa@gmail.com>
2023-10-26 20:05:54 +0300

    get ranking from the server

--- miniprokkis-front/src/components/TextField.tsx
+++ miniprokkis-front/src/components/TextField.tsx
@@ -1,57 +1,65 @@
-import { TextField as TextF, styled } from "@mui/material";
-import { Dispatch, useEffect, useState } from "react";
-import { rankResponseSchema } from "../types/rank";
+import { TextField as TextF, styled } from '@mui/material'
+import { Dispatch, useEffect, useState } from 'react'
+import { rankResponseSchema } from '../types/rank'
 
 interface TextFieldProps {
-    setRank: Dispatch<number>
+  setRank: Dispatch<number>
 }
 
-
 const StyledTextField = styled(TextF)({
-    '& label.Mui-focused': {
-        color: 'black',
+  '& label.Mui-focused': {
+    color: 'black',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'black',
+  },
+  '& .MuiOutlinedInput-root': {
+    '&.Mui-focused fieldset': {
+      borderColor: 'black',
     },
-    '& .MuiInput-underline:after': {
-        borderBottomColor: 'black',
+    '& fieldset': {
+      borderColor: 'black',
     },
-    '& .MuiOutlinedInput-root': {
-        '&.Mui-focused fieldset': {
-            borderColor: "black",
-        },
-        '& fieldset': {
-            borderColor: 'black',
-        },
-        '&:hover fieldset': {
-            borderColor: 'black',
-        },
-
+    '&:hover fieldset': {
+      borderColor: 'black',
     },
-});
+  },
+})
 
 const TextField = ({ setRank }: TextFieldProps) => {
+  const [text, setText] = useState<string>('')
+  console.log(text)
 
-    const [text, setText] = useState<string>('');
-
-    useEffect(() => {
-        if (text.length > 0) {
-            const delayReq = setTimeout(async () => {
-                const getRandomRank = async () => {
-                    const data = await fetch(`http://127.0.0.1:5000/randrank`);
-                    const validated = await rankResponseSchema.validate(await data.json());
-                    return validated.rank;
-                };
-                setRank(await getRandomRank());
-            }, 250);
-            return () => clearTimeout(delayReq);
+  useEffect(() => {
+    if (text.length > 0) {
+      const delayReq = setTimeout(async () => {
+        const getRank = async () => {
+          const response = await fetch(`http://127.0.0.1:5000/rank`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text: text }),
+          })
+          const validated = await rankResponseSchema.validate(
+            await response.json()
+          )
+          return validated.rank
         }
-        setRank(0);
-        return;
-    }, [text]);
+        setRank(await getRank())
+      }, 250)
+      return () => clearTimeout(delayReq)
+    }
+    setRank(0)
+    return
+  }, [text])
 
-    return (
-        <StyledTextField onChange={(event) => setText(event.target.value)} placeholder={"Enter your title"} />
-    );
-};
+  return (
+    <StyledTextField
+      onChange={(event) => setText(event.target.value)}
+      placeholder={'Enter your title'}
+    />
+  )
+}
 
-
-export default TextField;
+export default TextField
