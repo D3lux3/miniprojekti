f60a598154f65b6a544df28b04d1998d836d54ba
vesa <vesa@pop-os.localdomain>
2023-10-28 19:21:54 +0300

    round sum of title rank

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def rank_title(input):
     print(input)
     input = re.sub(r"[^\w\s]", "", input.lower()) # remove punctuations and change to lower
     stemmed_words = [PorterStemmer().stem(word) for word in str(input).split()] # stemming
-    return sum(word_rank.get(word, 0) for word in stemmed_words)
+    return round(sum(word_rank.get(word, 0) for word in stemmed_words), 2)
 
 app = Flask(__name__)
 CORS(app, origins="http://localhost:5173", supports_credentials=True)
